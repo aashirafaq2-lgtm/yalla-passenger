bdf8b41d8cfabe7a9d5f3801457719c65a60a5ec
aashirafaq2-igtm <aashirafaq@gmail.com>
2026-07-30 01:08:12 +0500

    Add cupertino import and pin font_awesome_flutter: 10.8.0 via dependency_overrides

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'core/theme/app_colors.dart';
