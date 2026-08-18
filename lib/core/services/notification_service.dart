62e7b918e529b8af189e80435e150cd6c79d326f
aashirafaq2-igtm <aashirafaq@gmail.com>
2026-08-18 22:16:57 +0500

    Fix white screen: add iOS notifications init, rename app to Yalla, update icon

--- lib/core/services/notification_service.dart
+++ lib/core/services/notification_service.dart
@@ -9,8 +9,15 @@ class NotificationService {
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
 
+    const DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+            requestAlertPermission: true,
+            requestBadgePermission: true,
+            requestSoundPermission: true);
+
     const InitializationSettings initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid,
+      iOS: initializationSettingsIOS,
     );
 
     await _notificationsPlugin.initialize(
